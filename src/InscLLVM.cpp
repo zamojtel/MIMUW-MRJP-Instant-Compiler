42138559be2ef75df88e22f3c18533c5f5e1997e
Bartek Zamojtel <zamojtelbartek@gmail.com>
2024-11-07 17:54:04 +0100

    Change the template for the llvm code generator

--- src/InscLLVM.cpp
+++ src/InscLLVM.cpp
@@ -48,19 +48,10 @@ int main(int argc, char ** argv)
   generate_code_llvm(parse_tree,data_llvm);
 
   constexpr const char * start_llvm = R"abc(
-; ModuleID = 'Instant'
-source_filename = "Instant"
-target datalayout = "e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-i128:128-f80:128-n8:16:32:64-S128"
-target triple = "x86_64-pc-linux-gnu"
-
-module asm ".globl _ZSt21ios_base_library_initv"
-
-@.str = private unnamed_addr constant [4 x i8] c"%d\0A\00", align 1
 
 @dnl = internal constant [4 x i8] c"%d\0A\00"
 
-; Function Attrs: mustprogress noinline norecurse optnone uwtable
-define dso_local noundef i32 @main() #0 {{)abc";
+define dso_local noundef i32 @main() {{)abc";
   
   std::string end_llvm= R"abc(
     ret i32 0
@@ -72,20 +63,7 @@ define void @printInt(i32 %x) {
        call i32 (i8*, ...) @printf(i8* %t0, i32 %x) 
        ret void
 }
-
-attributes #0 = { mustprogress noinline norecurse optnone uwtable "frame-pointer"="all" "min-legal-vector-width"="0" "no-trapping-math"="true" "stack-protector-buffer-size"="8" "target-cpu"="x86-64" "target-features"="+cmov,+cx8,+fxsr,+mmx,+sse,+sse2,+x87" "tune-cpu"="generic" }
-attributes #1 = { "frame-pointer"="all" "no-trapping-math"="true" "stack-protector-buffer-size"="8" "target-cpu"="x86-64" "target-features"="+cmov,+cx8,+fxsr,+mmx,+sse,+sse2,+x87" "tune-cpu"="generic" }
-
-!llvm.module.flags = !{!0, !1, !2, !3, !4}
-!llvm.ident = !{!5}
-
-!0 = !{i32 1, !"wchar_size", i32 4}
-!1 = !{i32 8, !"PIC Level", i32 2}
-!2 = !{i32 7, !"PIE Level", i32 2}
-!3 = !{i32 7, !"uwtable", i32 2}
-!4 = !{i32 7, !"frame-pointer", i32 2}
-!5 = !{!"Ubuntu clang version 18.1.3 (1ubuntu1)"}
-  )abc";
+)abc";
 
   std::string llvm_code;
   for(size_t i=0;i<data_llvm.m_lines.size();i++)
@@ -105,12 +83,10 @@ attributes #1 = { "frame-pointer"="all" "no-trapping-math"="true" "stack-protect
   std::string path_to_result_file = path_to_file+".bc"; 
   std::string run_llvm = "llvm-as "+path_to_baz_file+" -o "+path_to_result_file;
 
-  std::cout<<run_llvm<<std::endl;
+  // std::cout<<run_llvm<<std::endl;
   system(run_llvm.data());
-  /* Link it */
-  // std::string llvlinkCallString = fmt::format("llvm-link -o {} ./lib/runtime.bc",path_to_result_file,cwd);
   std::string run_llvm_result = "lli "+path_to_result_file;
-  std::cout<<run_llvm_result<<std::endl;
+  // std::cout<<run_llvm_result<<std::endl;
   system(run_llvm_result.data());
 
   if (parse_tree)
